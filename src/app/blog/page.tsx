1d054042dd47cb4989198659bbd0eb12e386ab08
GKastet <127600582+GKastet@users.noreply.github.com>
2024-01-11 12:54:32 +0100

    metadata absolute, default, template

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  // title: "Blog"
+  title: {
+    absolute: "Blog", //ignore template from layout
+  },
+};
+
 export default function Blog() {
   return <h1>My blog</h1>;
 }
