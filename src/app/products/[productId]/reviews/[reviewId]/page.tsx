58c0d4961422d3c7dc8fdb6d31e1a89bfdf39302
GKastet <127600582+GKastet@users.noreply.github.com>
2024-01-12 19:03:18 +0100

    error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -15,11 +15,11 @@ export default function ReviewDetail({
     reviewId: string;
   };
 }) {
-  const random = getRandomInt(2)
+  // const random = getRandomInt(2)
 
-  if(random === 1) {
-    throw new Error("Error loading review")
-  }
+  // if(random === 1) {
+  //   throw new Error("Error loading review")
+  // }
 
 
   if (parseInt(params.reviewId) > 1000) {
